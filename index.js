0f128f7bb6bcb56e88be71350eab5c8ad0232048
abhay <abhay@cateina.com>
2024-06-24 12:12:54 +0530

    completed

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.send("working!");
+});
+
+const db = require("./models");
+const Role = db.role;
+
+db.sequelize.sync();
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+
+// routes
+require("./routes/auth.route")(app);
+require("./routes/user.route")(app);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
---
+++ middlewares/index.js
@@ -0,0 +1,7 @@
+const authJwt = require("./authJwt");
+const verifySignUp = require("./verifyUser");
+
+module.exports = {
+  authJwt,
+  verifySignUp,
+};
